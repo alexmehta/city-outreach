132078e8809f39b0c6888cd9128b8f5799988b93
Alexander Mehta <53285116+alexmehta@users.noreply.github.com>
2021-04-16 11:54:04 -0700

    backup spriung boot

---
+++ SpringImplementation/src/main/java/com/implementation/demo/Rest.java
@@ -0,0 +1,15 @@
+package com.implementation.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Rest {
+    @PostMapping("/update")
+    public String getvalues(){
+
+
+    }
+}
---
+++ SpringImplementation/src/main/java/com/implementation/demo/SeliniumHaywardApplication.java
@@ -0,0 +1,13 @@
+package com.implementation.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeliniumHaywardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeliniumHaywardApplication.class, args);
+    }
+
+}
