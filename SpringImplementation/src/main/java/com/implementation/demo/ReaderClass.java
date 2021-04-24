4b090a48b8a5e98702918d704e2785feb54337f7
Alexander Mehta <53285116+alexmehta@users.noreply.github.com>
2021-04-23 22:43:01 -0700

    db update

--- SpringImplementation/src/main/java/com/implementation/demo/ReaderClass.java
+++ SpringImplementation/src/main/java/com/implementation/demo/ReaderClass.java
@@ -7,41 +7,55 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.sql.Date;
-import java.sql.Time;
+import java.sql.*;
 import java.util.Scanner;
 
 public class ReaderClass {
     @Autowired
     BasicRepo basicRepo;
-    public int RW(File file) throws IOException {
+    public int RW(File file) throws IOException, SQLException {
         long lines = Files.lines(file.toPath()).count();
         Scanner s = new Scanner(file);
-        int st = (int) (lines / 7);
+        int number = (int) (lines / 7);
         PrintWriter printWriter = new PrintWriter(new File("src/main/tmp/test.txt"));
-        for (int i = 0; i < st; i++) {
+        for (int i = 0; i < number; i++) {
             String[] string = new String[7];
             for (int j = 0; j < 7; j++) {
                 String s1 = s.nextLine();
                 string[i] = s1;
                 printWriter.println(s1);
             }
-            EntityEvent event = new EntityEvent();
 
-
-            basicRepo.save(event);
-            event.setName(string[0]);
-            event.setDate(string[1]);
-            event.setTime(string[2]);
-            event.setLocation(string[3]);
-            event.setPresentations(string[4]);
-            event.setDocuments(string[5]);
-            event.setOfficalmin(string[6]);
-            event.setId(event.getId());
-            printWriter.println();
+            Connection con = null;
+            String url = "jdbc:mysql://localhost:6033/";
+            String db = "cityofhayward";
+            String driver = "com.mysql.jdbc.Driver";
+            String user = "devuser";
+            String pass = "devpass";
+            try{
+                Class.forName(driver).newInstance();
+                con = DriverManager.getConnection(url+db, user, pass);
+                Statement st = con.createStatement();
+                ResultSet res = st.executeQuery("INSERT INTO FROM  employee6");
+                System.out.println("Emp_code: " + "\t" + "Emp_name: ");
+                while (res.next()) {
+                    int code = res.getInt("Emp_code");
+                    String sring = res.getString("Emp_name");
+                    System.out.println(i + "\t\t" + sring);
+                }
+            }
+            catch (SQLException srings){
+                System.out.println("SQL code does not execute.");
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            finally {
+                con.close();
+            }
         }
 
         printWriter.close();
-        return st;
+        return number;
     }
 }
