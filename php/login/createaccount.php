9613f490ac5aaae25ea3ad8cab7084af1c91d5bd
Alex Mehta <you@example.com>
2021-04-25 11:43:26 -0700

    quickfix for login

--- php/login/createaccount.php
+++ php/login/createaccount.php
@@ -1,7 +1,3 @@
-<?php
-
-
-?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -9,11 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>City of Hayward Connect</title>
+    <title>Sign up</title>
 </head>
 <body>
-    <a href="login/createaccount.php">Create Account</a>
-    <br>
-    <a href="login/login.html">Login</a>
+<form action="createUser.php" method="post">
+    <label>Email</label>
+    <input name="email" type="email">
+    <label>Password</label>
+    <input type="password" name="password">
+    <input type="submit">
+</form>
 </body>
 </html>
--- php/user/User.php
+++ php/user/User.php
@@ -1,38 +1,39 @@
 <?php
-
-
 class User
 {
-    function BasicUser($email, $password){
+    function BasicUser($email, $password)
+    {
         ini_set('display_errors', 1);
-
         include "../includes/includes.php";
         $sql = "INSERT INTO users (email, password) values(?,?)";
         $stmt = $pdo->prepare($sql);
-        $stmt->execute([$email,$password]);
-
+        $stmt->execute([$email, $password]);
     }
-    function login($email,$password){
+
+    function login($email, $password)
+    {
         ini_set('display_errors', 1);
         include "../includes/includes.php";
+        session_start();
         $sql = "SELECT * FROM users WHERE email = ? LIMIT 1";
         $stmt = $pdo->prepare($sql);
         $stmt->execute([$email]);
-        $stmt= $stmt->fetch();
-        $id = $stmt['id'];
-
-        if ($password==$stmt['password']&&$email==$stmt['email']){
-            session_start();
-            $_SESSION['id'] = $id;
-            $_SESSION['email'] = $stmt['email'];
-            $_SESSION['loged'] = true;
-            $_COOKIE['last_login'] = date("Y/m/d");
-            header("LOCATION: ../index.php");
+        $stmt = $stmt->fetch();
+        if ($stmt){
+            if (isset($password) && isset($email) && isset($stmt)) {
+                if ($password == $stmt['password']) {
+                    $id = $stmt['id'];
+                    $_SESSION['id'] = $id;
+                    $_SESSION['email'] = $stmt['email'];
+                    $_SESSION['loged'] = true;
+                    $_COOKIE['last_login'] = date("Y/m/d");
+                    header("LOCATION: ../index.php");
+                    exit();
+                }
+            }
         }
-        else{
-            header("LOCATION: ../login/error.html");
-        }
 
-
+        header("LOCATION: error.html");
+        exit();
     }
 }
