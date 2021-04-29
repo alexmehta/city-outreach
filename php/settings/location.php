3804f0e7e476ffbf58cb6aab8cea9932ec0aa54f
Alex Mehta <you@example.com>
2021-04-28 21:46:24 -0700

    bootstrap style

--- php/settings/location.php
+++ php/settings/location.php
@@ -8,70 +8,64 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Location</title>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<form style="padding: 10px" method="post" action="db/location.php">
 
-<form method="post" action="db/location.php">
+    <!-- Address form -->
 
-    <fieldset>
-        <!-- Address form -->
+    <h1>Address</h1>
 
-        <h1>Address</h1>
-
-        <!-- address-line1 input-->
-        <div class="control-group">
-            <label class="control-label">Address Line 1</label>
-            <div class="controls">
-                <input id="address-line1" name="address-line1" type="text" placeholder="address line 1"
-                       class="input-xlarge">
-                <p class="help-block">Street address, P.O. box, company name, c/o</p>
-            </div>
+    <!-- address-line1 input-->
+    <div class="form-group">
+        <label class="control-label">Address Line 1</label>
+        <div class="controls">
+            <input id="address-line1" name="address-line1" type="text" placeholder="address line 1"
+                   class="input-xlarge">
+            <p class="help-block">Street address, P.O. box, company name, c/o</p>
         </div>
-        <!-- address-line2 input-->
-        <div class="control-group">
-            <label class="control-label">Address Line 2</label>
-            <div class="controls">
-                <input id="address-line2" name="address-line2" type="text" placeholder="address line 2"
-                       class="input-xlarge">
-                <p class="help-block">Apartment, suite , unit, building, floor, etc.</p>
-            </div>
+    </div>
+    <!-- address-line2 input-->
+    <div class="form-group">
+        <label >Address Line 2</label>
+        <div class="controls">
+            <input id="address-line2" name="address-line2" type="text" placeholder="address line 2"
+                   >
+            <p class="help-block">Apartment, suite , unit, building, floor, etc.</p>
         </div>
-        <!-- city input-->
-        <div class="control-group">
-            <label class="control-label">City / Town</label>
-            <div class="controls">
-                <input id="city" name="city" type="text" placeholder="city" class="input-xlarge">
-                <p class="help-block"></p>
-            </div>
+    </div>
+    <!-- city input-->
+    <div class="form-group">
+        <label class="control-label">City / Town</label>
+        <div class="controls">
+            <input id="city" name="city" type="text" placeholder="city" class="input-xlarge">
+            <p class="help-block"></p>
         </div>
-        <!-- region input-->
-        <div class="control-group">
-            <label class="control-label">State / Province / Region</label>
-            <div class="controls">
-                <input id="region" name="region" type="text" placeholder="state / province / region"
-                       class="input-xlarge">
-                <p class="help-block"></p>
-            </div>
+    </div>
+    <!-- region input-->
+    <div class="form-group">
+        <label class="control-label">State / Province / Region</label>
+        <div>
+            <input id="region" name="region" type="text" placeholder="state / province / region"
+                   class="input-xlarge">
+            <p class="help-block"></p>
         </div>
-        <!-- postal-code input-->
-        <div class="control-group">
-            <label class="control-label">Zip / Postal Code</label>
-            <div class="controls">
-                <input id="postal-code" name="postal-code" type="text" placeholder="zip or postal code"
-                       class="input-xlarge">
-                <p class="help-block"></p>
-            </div>
+    </div>
+    <!-- postal-code input-->
+    <div class="form-group">
+        <label class="control-label">Zip / Postal Code</label>
+        <div class="controls">
+            <input id="postal-code" name="postal-code" type="text" placeholder="zip or postal code"
+                   class="input-xlarge">
+            <p class="help-block"></p>
         </div>
+    </div>
     <input aria-label="submit" placeholder="submit" name="submit" value="submit" type="submit">
-    </fieldset>
 
 </form>
 </body>
