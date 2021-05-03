af1b7c21e1485fc6a541ab05a14be278a282a030
Alex Mehta <orangespock@gmail.com>
2021-05-02 20:59:22 -0700

    100% accessablityu lighthouse.

--- php/index.php
+++ php/index.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['id'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=5.0, minimum-scale=0.8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>City of Hayward Connect</title>
     <!-- Latest compiled and minified CSS -->
