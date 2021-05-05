67acaa41fc3b43f7dbc141f72893ba823424c588
Alex Mehta <orangespock@gmail.com>
2021-05-04 19:17:16 -0700

    added header to changing view

--- php/settings/db/view.php
+++ php/settings/db/view.php
@@ -1,9 +1,13 @@
 <?php
 
-    session_start();
-    $answer = $_GET['answer'];
-    if ($answer=="key"){
-        $_SESSION['view'] = true;
-    }else{
-        $_SESSION['view'] = false;
-    }
+session_start();
+$answer = $_GET['answer'];
+if ($answer == "key") {
+    $_SESSION['view'] = true;
+} else {
+    $_SESSION['view'] = false;
+}
+header("LOCATION: ../../index.php");
+
+
+?>
