a2ec7c49fb990e4b38de854f79c7220a44e19da6
Alexander Mehta <53285116+alexmehta@users.noreply.github.com>
2021-05-13 20:00:09 -0700

    sql files

--- SQL/003-notifications.sql
+++ SQL/003-notifications.sql
@@ -2,9 +2,12 @@ create table if not exists notifications
 (
 	id int auto_increment,
 	eventid int not null,
-	sent tinyint(1) null,
-	timesent datetime null,
+	userid int not null,
+	sent tinyint(1) default 0 not null,
 	constraint notifications_id_uindex
 		unique (id)
 );
+
+alter table notifications
+	add primary key (id);
 
--- SQL/004-upcomingevents.sql
+++ SQL/004-upcomingevents.sql
@@ -3,13 +3,14 @@ create table if not exists upcomingevents
 	id int auto_increment
 		primary key,
 	name varchar(1000) null,
-	date varchar(200) null,
+	date varchar(800) null,
 	time varchar(3333) null,
 	location text null,
 	presentations text null,
 	documents text null,
 	officalmin text null,
-	tag varchar(423) null
+	tag varchar(423) null,
+	deleteable tinyint(1) default 1 null comment 'check if other events are being used with this'
 )
 comment 'upcoming events database';
 
