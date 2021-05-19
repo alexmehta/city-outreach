adb2e738a118bc48d63a84922b0c3981ccf6936f
Alexander Mehta <53285116+alexmehta@users.noreply.github.com>
2021-05-18 17:49:22 -0700

    sql

--- SQL/005-users.sql
+++ SQL/005-users.sql
@@ -2,7 +2,7 @@ create table if not exists users
 (
 	id int auto_increment,
 	email varchar(500) null,
-	password varchar(255) not null,
+	password varchar(255) null,
 	dob datetime null,
 	address1 text null,
 	address2 text null,
@@ -10,9 +10,14 @@ create table if not exists users
 	state varchar(50) null,
 	zip int null,
 	view tinyint(1) default 0 null,
-	name text null,
+	googleid varchar(500) null,
+	profile text null,
+	firstname text null,
+	lastname text null,
 	constraint users_email_uindex
 		unique (email),
+	constraint users_googleid_uindex
+		unique (googleid),
 	constraint users_id_uindex
 		unique (id)
 );
