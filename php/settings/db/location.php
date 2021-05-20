9649af30aabe6b61b732514dd6d24e631e868d21
Alex Mehta <orangespock@gmail.com>
2021-05-20 16:06:57 -0700

    csv and sql files

--- php/settings/db/location.php
+++ php/settings/db/location.php
@@ -1,6 +1,6 @@
 <?php
-    require "../../user/User.php";
-    session_start();
-    $user = new User();
-    $user->location($_POST['address-line1'],$_POST['address-line2'],$_POST['city'],$_POST['region'],$_POST['postal-code'],$_SESSION['id']);
-    header("LOCATION: ../../settings.php");
+require "../../user/User.php";
+session_start();
+$user = new User();
+$user->location($_POST['address-line1'], $_POST['address-line2'], $_POST['city'], $_POST['region'], $_POST['postal-code'], $_SESSION['id']);
+header("LOCATION: ../../settings.php");
