e24230f38c856cd0019a8573456658a17a062078
Alex Mehta <orangespock@gmail.com>
2021-05-20 15:57:18 -0700

    working after merge

--- php/settings/db/location.php
+++ php/settings/db/location.php
@@ -3,3 +3,4 @@
     session_start();
     $user = new User();
     $user->location($_POST['address-line1'],$_POST['address-line2'],$_POST['city'],$_POST['region'],$_POST['postal-code'],$_SESSION['id']);
+    header("LOCATION: ../../settings.php");
