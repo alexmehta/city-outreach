bb7b7c9e92658e19255d58b07b08c97f4daa39b7
Alexander Mehta <53285116+alexmehta@users.noreply.github.com>
2021-05-28 22:28:40 -0700

    it works, but not really

--- Spring/src/main/java/com/hayward/spring/events/PDFreader.java
+++ Spring/src/main/java/com/hayward/spring/events/PDFreader.java
@@ -1,17 +1,99 @@
 package com.hayward.spring.events;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.pdfbox.io.IOUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PDFreader {
-    //main runner class
-    public void attemptZoomLink(int id){
-        Update(parsePDF(), id);
+    private static byte[] readFileAsBytes(String filePath) throws IOException {
+        FileInputStream inputStream = new FileInputStream(filePath);
+        return IOUtils.toByteArray(inputStream);
     }
+
+    public static String convertPDFToTxt(String filePath) throws IOException {
+        byte[] thePDFFileBytes = readFileAsBytes(filePath);
+        PDDocument pddDoc = PDDocument.load(thePDFFileBytes);
+        PDFTextStripper reader = new PDFTextStripper();
+        String pageText = reader.getText(pddDoc);
+        pddDoc.close();
+        return pageText;
+    }
+
+    public static String getPDF(int id) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/cityofhayward";
+            conn = DriverManager.getConnection(url, "devuser", "devpass");
+            stmt = conn.createStatement();
+            String sql = "SELECT * FROM upcomingevents WHERE id = %s";
+            sql = String.format(sql, id);
+            System.out.println(sql);
+            ResultSet statement = stmt.executeQuery(sql);
+            while (statement.next()) {
+                FileUtils.copyURLToFile(new URL("https://hayward.legistar.com/" + statement.getString("pdf")), new File("pdf.pdf"));
+                return convertPDFToTxt("pdf.pdf");
+            }
+
+        } catch (Exception excep) {
+            excep.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    conn.close();
+            } catch (SQLException ignored) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return "not found";
+
+    }
+
     //go through pdf and try to find mentions of hayward.zoom.us
-    public static String parsePDF(){
-
+    public static String parsePDF(int id) {
+        String text = getPDF(id);
+        List<String> splitStr = Arrays.stream(text.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+        for (String s : splitStr) {
+            if (s.contains("hayward.zoom.us")) {
+                return s;
+            }
+        }
+        return "";
     }
-    //update the sql row
-    public static void Update(String url, int id){
 
+    //update the sql row
+    public static void Update(String url, int id) {
+        String query = "UPDATE upcomingevents SET zoomlink='%s' WHERE id=%s";
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cityofhayward", "devuser", "devpass");
+             Statement stmt = conn.createStatement()
+        ) {
+            stmt.executeUpdate(String.format(query, url,id));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //main runner class
+    public void attemptZoomLink(int id) {
+        Update(parsePDF(id), id);
     }
 
 }
